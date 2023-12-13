b2ea1bfebc6b55ea11f57a8fe749c2a64251682a
I Mercia Jeno <113104600+merciajeno@users.noreply.github.com>
2023-12-13 21:00:03 +0530

    Update JpaRunner.java

--- src/main/java/com/example/jpaPractice/course/controller/JpaRunner.java
+++ src/main/java/com/example/jpaPractice/course/controller/JpaRunner.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 import com.example.jpaPractice.course.Employee;
 import com.example.jpaPractice.course.repository.EmployeeRepo;
 
-//@Component
+//@Component If you want to use this class,uncomment the line
 public class JpaRunner implements CommandLineRunner{
 
 	//@Autowired
@@ -18,10 +18,10 @@ public class JpaRunner implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-//		repository.save(new Employee(1,"Mercia",new BigDecimal(324242.23),LocalDate.of(2023, 12, 10)));
-//		repository.save(new Employee(2,"Shania",new BigDecimal(324454),LocalDate.of(2021, 12, 10)));
-//		repository.save(new Employee(3,"Issac",new BigDecimal(4254524),LocalDate.of(2013, 12, 10)));
-//		repository.save(new Employee(4,"Merline",new BigDecimal(3234533),LocalDate.of(2020, 12, 10)));
+		repository.save(new Employee(1,"Mercia",new BigDecimal(324242.23),LocalDate.of(2023, 12, 10)));
+		repository.save(new Employee(2,"Shania",new BigDecimal(324454),LocalDate.of(2021, 12, 10)));
+		repository.save(new Employee(3,"Issac",new BigDecimal(4254524),LocalDate.of(2013, 12, 10)));
+		repository.save(new Employee(4,"Merline",new BigDecimal(3234533),LocalDate.of(2020, 12, 10)));
 		System.out.println(repository.findAll());
 		System.out.println(repository.findById(1));
 		
